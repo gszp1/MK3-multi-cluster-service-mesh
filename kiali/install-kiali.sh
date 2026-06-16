4488756522c0b923d35fbf34f33659205cd49c45
gszp1 <grzybekmateusz@poczta.fm>
2026-06-16 18:26:45 +0200

    Add Kiali for cluster view

---
+++ kiali/install-kiali.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+KUBECONFIG_FILE="$SCRIPT_DIR/../kubeconfig/kubeconfig.yaml"
+export KUBECONFIG="$KUBECONFIG_FILE"
+
+CTX="kind-primary"
+
+helm repo add kiali https://kiali.org/helm-charts
+
+helm repo update
+
+helm upgrade --install \
+  --kube-context="$CTX" \
+  --namespace istio-system \
+  --set auth.strategy="anonymous" \
+  --set server.web_port=20001 \
+  --set deployment.service_type=LoadBalancer \
+  --repo https://kiali.org/helm-charts \
+  kiali-server \
+  kiali-server
